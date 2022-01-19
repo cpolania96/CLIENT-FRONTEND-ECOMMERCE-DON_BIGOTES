9aa517f3a8b661dc3bd72fe7eaf0588afef163a5
Camilo Polania <cpolania@outlook.com>
2022-01-18 23:37:30 -0500

    Termina trabajo 18/01/2022 11:37pm

--- proyecto/tienda_virtual-donbigotes/src/modules/Detail/ContainerInfo.jsx
+++ proyecto/tienda_virtual-donbigotes/src/modules/Detail/ContainerInfo.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import IconInfo from '../svg/IconInfo'
+import IconMessages from '../svg/IconMessages'
 
 function ContainerInfo() {
+    const titleInfo = "Información del producto"
+    const titleComments = "Opiniones y comentarios"
+    let productInfo = "info"
     return (
-        <div className="detail">
-            <div className='comments'></div>
-            <div className='info'>
+        <div className="container-comments">
+            <div className='comments'>
+                <h6><span><IconMessages /></span>{titleComments}</h6>
+                <div className="comments-body">
+
+                </div>
+
+            </div>
+            <div className='info-product'>
+                <h6><span><IconInfo /></span>{titleInfo}</h6>
+                <div className='info'>
+                    <div className='info-title'>
+                        <div className="title"></div>
+                        <div className="title"></div>
+                        <div className="title"></div>
+                    </div>
+                    <div></div>
+                </div>
             </div>
         </div>
 
