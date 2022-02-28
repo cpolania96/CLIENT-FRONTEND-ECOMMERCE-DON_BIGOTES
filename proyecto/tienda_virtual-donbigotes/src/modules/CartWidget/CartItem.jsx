b5fc37b972cf8f3aea7edd73fdd6fa510f591557
Camilo Polania <cpolania@outlook.com>
2022-02-28 18:38:34 -0500

    Correccion en directorios

---
+++ proyecto/tienda_virtual-donbigotes/src/modules/CartWidget/CartItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ItemCount from '../ItemCount/ItemCount';
+
+function CartItem({ item, borrarItem }) {
+    const imageURL = "https://res.cloudinary.com/devsy44f3/image/upload/v1643168330/Products/Mesa_de_trabajo_2_l8bbxg.png"
+    return (
+        <div className='cart-item'>
+            <div className="image">
+                <img src={item.imageURL} alt="" />
+            </div>
+            <div className="container-descript">
+                <div className="name">{item.title}</div>
+                <div className="descript">Descripción</div>
+            </div>
+            <div className="price">
+                {/* <ItemCount />   | */}
+                <h6>${item.price.toLocaleString('de-DE')}</h6>
+            </div>
+            <button className='close' onClick={() => borrarItem(item.id)} > X </button>
+        </div>
+    )
+}
+
+export default CartItem
